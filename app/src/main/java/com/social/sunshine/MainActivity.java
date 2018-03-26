4cf19e306d4ce6ac9efff4c710fd8f96fa74fe0c
= <ousmanefouchimi@gmail.com>
2018-03-25 18:08:47 -0600

    Implemented countUp method

--- app/src/main/java/com/social/sunshine/MainActivity.java
+++ app/src/main/java/com/social/sunshine/MainActivity.java
@@ -3,14 +3,18 @@ package com.social.sunshine;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         mShowCount = findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
@@ -19,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
-
+        mCount++;
+        if(mShowCount != null) {
+            mShowCount.setText(mCount);
+        }
     }
 }
